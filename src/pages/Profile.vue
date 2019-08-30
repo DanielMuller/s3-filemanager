8c083e1f6abc5954428c202104f618ea0853cbbd
Daniel Muller <daniel@spuul.com>
2019-08-30 16:48:24 +0800

    Add login page

---
+++ src/pages/Profile.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  q-page(padding)
+    h1 Welcome {{ user.username }}
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  data () {
+    return {
+      user: null
+    }
+  },
+  created () {
+    this.$Auth.currentAuthenticatedUser()
+      .then(user => {
+        this.user = user
+      })
+  }
+}
+</script>
+
+<style>
+</style>
