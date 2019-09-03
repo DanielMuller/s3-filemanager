dda96d72011296fac1af8fdb926bbd7c11e67d87
Daniel Muller <daniel@spuul.com>
2019-09-03 10:54:26 +0800

    Use eva-icons and handle folder click

--- quasar.conf.js
+++ quasar.conf.js
@@ -17,7 +17,7 @@ module.exports = function (ctx) {
       // 'ionicons-v4',
       // 'mdi-v3',
       // 'fontawesome-v5',
-      // 'eva-icons',
+      'eva-icons',
       // 'themify',
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
@@ -41,6 +41,7 @@ module.exports = function (ctx) {
         'QToolbarTitle',
         'QBtn',
         'QIcon',
+        'QAvatar',
         'QList',
         'QItem',
         'QItemSection',
