107dc26cc29fe5894b59e56f05fc8879457aaabb
Daniel Muller <daniel@spuul.com>
2019-09-03 12:17:12 +0800

    Use Amplify.Logger

--- src/boot/amplify.js
+++ src/boot/amplify.js
@@ -14,6 +14,11 @@ export default async ({
   Vue.prototype.$Amplify = Amplify
   Vue.prototype.$AmplifyEventBus = AmplifyEventBus
   Vue.prototype.$Auth = AmplifyModules.Auth
+  let logLevel = 'INFO'
+  if (process.env.DEV) {
+    logLevel = 'DEBUG'
+  }
+  Vue.prototype.$Logger = new AmplifyModules.Logger('app', logLevel)
 
   router.beforeResolve((to, from, next) => {
     if (to.matched.some(record => record.meta.requiresAuth)) {
