ed5c888080f4d60f1bdfe40b091f5299e267b828
Daniel Muller <daniel@spuul.com>
2019-11-08 12:43:59 +0800

    Manage Users

--- src/pages/Admin/Users.vue
+++ src/pages/Admin/Users.vue
@@ -3,18 +3,25 @@
     .text-h5 Users
     q-btn(rounded color="positive" icon="add_circle_outline" no-caps :to="{name:'adminNewUser'}") Create User
     .q-pa-md.row.items-start.q-gutter-md
-      q-card(v-for="user in users" :key="user.Username")
+      q-card(v-for="user in users" :key="user.Username" :class="userStatus(user)")
         q-card-section
           .text-h6 {{ user.Username }}
             q-icon(name="person" color="negative" v-if="user.isAdmin")
-          .text-subtitle2 {{ userEmail(user) }}
-            q-icon(name="verified_user" color="positive" v-if="verifiedEmail(user)")
-            q-icon(name="error_outline" color="negative" v-if="!verifiedEmail(user)")
+          .text-subtitle2 {{ user.email }}
+            q-icon(name="verified_user" color="positive" v-if="user.email_verified")
+            q-icon(name="error_outline" color="negative" v-if="!user.email_verified")
         q-card-section
           .text-body2 Created: {{ dateFormat(user.UserCreateDate) }}
           .text-body2 Modified: {{ dateFormat(user.UserLastModifiedDate) }}
           .text-body2 Status: {{ user.UserStatus }}
           .text-body2 Enabled: {{ user.Enabled }}
+        q-card-actions(align="right")
+          q-btn(v-if="user.Enabled" flat round icon="remove_circle" text-color="grey" :disabled="mySelf(user)" @click="setStatus(user, 'disabled')")
+            q-tooltip Disable User
+          q-btn(v-else flat round icon="add_circle" text-color="positive" :disabled="mySelf(user)" @click="setStatus(user, 'enabled')")
+            q-tooltip Enable User
+          q-btn(v-if="!user.Enabled" flat round icon="delete" text-color="negative" :disabled="mySelf(user)" @click="deleteUser(user)")
+            q-tooltip Delete User
 </template>
 
 <script>
@@ -23,13 +30,44 @@ export default {
   name: 'AdminUsers',
   data () {
     return {
-      users: []
+      users: [],
+      signedUser: null
     }
   },
   created () {
     this.listUsers()
   },
+  beforeCreate () {
+    this.$Auth.currentAuthenticatedUser()
+      .then(user => {
+        this.signedUser = user
+        // this.username = user.username
+        // this.email = user.attributes.email
+        // this.email_verified = user.attributes.email_verified
+      })
+      .catch(err => { // eslint-disable-line handle-callback-err
+      })
+  },
   methods: {
+    async setStatus (user, status) {
+      let apiName = 'AdminQueries'
+      let path = (status === 'enabled') ? '/enableUser' : '/disableUser'
+      let myInit = {
+        body: {
+          'username': user.Username
+        },
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${(await this.$Auth.currentSession()).getAccessToken().getJwtToken()}`
+        }
+      }
+      await this.$Amplify.API.post(apiName, path, myInit)
+      for (let i = 0; i < this.users.length; i++) {
+        if (this.users[i].Username === user.Username) {
+          this.users[i].Enabled = (status === 'enabled')
+        }
+      }
+    },
     async listUsers (limit = 10, nextToken = '') {
       let apiName = 'AdminQueries'
       let path = '/listUsers'
@@ -53,8 +91,44 @@ export default {
         }
         rest.Users[i].isAdmin = isAdmin
       }
-      this.users = rest.Users
+      this.users = this.setUserAttributes(rest.Users)
+      // this.users = rest.Users
       return rest
+    },
+    setUserAttributes (users) {
+      for (let i = 0; i < users.length; i++) {
+        users[i].Attributes.forEach(att => {
+          users[i][att.Name] = att.Value
+        })
+      }
+      return users
+    },
+    async deleteUser (user) {
+      let apiName = 'AdminQueries'
+      let path = '/deleteUser'
+      let myInit = {
+        body: {
+          'username': user.Username
+        },
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${(await this.$Auth.currentSession()).getAccessToken().getJwtToken()}`
+        }
+      }
+      await this.$Amplify.API.post(apiName, path, myInit)
+      let indexToRemove = -1
+      for (let i = 0; i < this.users.length; i++) {
+        if (this.users[i].Username === user.Username) {
+          indexToRemove = i
+        }
+      }
+      this.users.splice(indexToRemove, 1)
+    },
+    mySelf (user) {
+      return this.signedUser.attributes.sub === user.sub
+    },
+    userStatus (user) {
+      return (user.Enabled) ? '' : 'bg-red-2'
     },
     async listGroupsForUser (username) {
       let apiName = 'AdminQueries'
@@ -70,12 +144,6 @@ export default {
       }
       const { NextToken, ...rest } = await this.$Amplify.API.get(apiName, path, myInit)
       return rest.Groups.map(item => item.GroupName)
-    },
-    userEmail (user) {
-      return user.Attributes.filter(att => att.Name === 'email')[0].Value
-    },
-    verifiedEmail (user) {
-      return (user.Attributes.filter(att => att.Name === 'email_verified')[0].Value === 'true')
     },
     dateFormat (dateString) {
       let now = new Date()
